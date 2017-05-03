5d6596edc4d0f57a1a722a8a9df2b5a230bf8e6d
a05h <a117q35mt@gmail.com>
2017-05-03 17:18:26 -0400

    startpage init

---
+++ v/startpage/source/javascripts/main.js
@@ -0,0 +1,59 @@
+'use strict';
+
+function getSearchlineFocus() {
+  document.getElementById('search-line').focus();
+};
+
+function loadBookmarks() {
+  let bookmarksDB = JSON.parse(bookmarks),
+      linksList = document.getElementById('links-list');
+  
+  function cutDescription(description) {
+    let cuttedDescription = description;
+    if (description.length > 35) {
+      cuttedDescription = `${description.substr(0, 32)} ...`;
+    }
+    return cuttedDescription;
+  };
+  
+  for (let i = 0; i < bookmarksDB.length; i++) {
+    linksList.innerHTML += `<a href="${bookmarksDB[i].url}">${cutDescription(bookmarksDB[i].desc)}</a><br>`;
+  }
+};
+
+
+let bookmarksIsShown = false;
+function bookmarksToggle() {
+  let bookmarksPanel = document.getElementById('bookmarks-panel');
+  
+  if (!bookmarksIsShown) {
+    bookmarksPanel.style.visibility = "visible";
+    bookmarksPanel.style.opacity = "1";
+    bookmarksPanel.style.right = "60px";
+    bookmarksIsShown = true;
+  } else {
+    bookmarksPanel.style.opacity = "0";
+    bookmarksPanel.style.visibility = "hidden";
+    bookmarksPanel.style.right = "50px";
+    bookmarksIsShown = false;
+  } 
+};
+
+
+let sengineIsShown = false;
+function sengineToggle() {
+  let senginePanel = document.getElementById('searchengine-panel');
+  if (!sengineIsShown) {
+    senginePanel.style.visibility = "visible";
+    senginePanel.style.opacity = "1";
+    senginePanel.style.right = "60px";
+    sengineIsShown = true;
+  } else {
+    senginePanel.style.opacity = "0";
+    senginePanel.style.visibility = "hidden";
+    senginePanel.style.right = "50px";
+    sengineIsShown = false;
+  } 
+};
+
+
