5cc9586498c46ba71ac0244075d5ae83764b1087
a05h <a117q35mt@gmail.com>
2017-05-09 15:52:56 -0400

    sp update

--- v/startpage/source/javascripts/main.js
+++ v/startpage/source/javascripts/main.js
@@ -8,7 +8,6 @@ function getSearchlineFocus() {
 function loadBookmarks() {
   let bookmarksDB = JSON.parse(collection),
       linksList = document.getElementById('links-list');
-  
   function cutDescription(description) {
     let cuttedDescription = description;
     if (description.length > 35) {
@@ -16,31 +15,9 @@ function loadBookmarks() {
     }
     return cuttedDescription;
   };
-  
   for (let i = 0; i < bookmarksDB.length; i++) {
     linksList.innerHTML += `<a href="${bookmarksDB[i].url}">${cutDescription(bookmarksDB[i].desc)}</a><br>`;
   }
-};
-
-
-let popupIsShown = false;
-function newBookmarkToggle() {
-  let popupWindow = document.getElementById('new-bookmark-window');
-  if (!popupIsShown) {
-    popupWindow.style.visibility = 'visible';
-    popupWindow.style.opacity = '1';
-    popupIsShown = true;
-  } else {
-    popupWindow.style.opacity = '0';
-    popupWindow.style.visibility = 'hidden';
-    popupIsShown = false;
-  }
-};
-
-
-function clearInputs() {
-  document.getElementById('bookmark-description').value = '';
-  document.getElementById('bookmark-url').value = '';
 };
 
 
@@ -78,3 +55,138 @@ function sengineToggle() {
 };
 
 
+function hidePanels() {
+  let bookmarksPanel = document.getElementById('bookmarks-panel'),
+      senginePanel = document.getElementById('searchengine-panel');
+  bookmarksPanel.style.opacity = '0';
+  bookmarksPanel.style.visibility = 'hidden';
+  bookmarksPanel.style.right = '50px';
+  senginePanel.style.opacity = '0';
+  senginePanel.style.visibility = 'hidden';
+  senginePanel.style.right = '50px';
+  bookmarksIsShown = false;
+  sengineIsShown = false;
+};
+
+
+let currentSearchEngine = 'Google';
+function changeSearchEngine(searchEngine = google) {
+  let form = document.getElementById('search-form'),
+      line = document.getElementById('search-line'),
+      title = document.getElementById('searchengine-title');
+  switch (searchEngine) {
+    case 'google':
+      form.action = 'http://www.google.co.uk/search';
+      form.method = 'get';
+      line.name = 'q';
+      title.innerHTML = 'Google';
+      title.style.color = '#7bbbe3';
+      title.style.fontFamily = "'Lobster', cursive";
+      title.style.margin = '-50px 0 0 55px';
+      title.style.fontSize = '76px';
+      title.style.fontWeight = '300';
+      document.title = 'Google';
+      currentSearchEngine = 'Google';
+      break;
+    case 'duckDuckGo':
+      form.action = 'https://www.duckduckgo.com';
+      form.method = 'get';
+      line.name = 'q';
+      title.innerHTML = 'DuckDuckGo';
+      title.style.color = '#e3af7b';
+      title.style.fontFamily = "'Courgette', cursive";
+      title.style.margin = '-40px 0 0 -15px';
+      title.style.fontSize = '62px';
+      title.style.fontWeight = '400';
+      document.title = 'DuckDuckGo';
+      currentSearchEngine = 'DuckDuckGo';
+      break;
+    case 'proxy':
+      form.action = 'https://eu2.proxysite.com/includes/process.php?action=update';
+      form.method = 'post';
+      line.name = 'd';
+      title.innerHTML = 'Proxy';
+      title.style.color = '#e37b7b';
+      title.style.fontFamily = "'Play', sans-serif";
+      title.style.margin = '-50px 0 0 63px';
+      title.style.fontSize = '76px';
+      title.style.fontWeight = '300';
+      document.title = 'Proxy';
+      currentSearchEngine = 'Proxy';
+      break;
+  }
+  hidePanels();
+  getSearchlineFocus();
+};
+
+
+function changeProxyServer(server = 'eu2') {
+  if (currentSearchEngine === 'Proxy') {
+    let form = document.getElementById('search-form');
+    switch (server) {
+      case 'eu1':
+        form.action = 'https://eu1.proxysite.com/includes/process.php?action=update';
+        break;
+      case 'eu2':
+        form.action = 'https://eu2.proxysite.com/includes/process.php?action=update';
+        break;
+      case 'eu3':
+        form.action = 'https://eu3.proxysite.com/includes/process.php?action=update';
+        break;
+      case 'eu4':
+        form.action = 'https://eu4.proxysite.com/includes/process.php?action=update';
+        break;
+      case 'eu5':
+        form.action = 'https://eu5.proxysite.com/includes/process.php?action=update';
+        break;
+      case 'eu6':
+        form.action = 'https://eu6.proxysite.com/includes/process.php?action=update';
+        break;
+      case 'us1':
+        form.action = 'https://us1.proxysite.com/includes/process.php?action=update';
+        break;
+      case 'us2':
+        form.action = 'https://us2.proxysite.com/includes/process.php?action=update';
+        break;
+      case 'us3':
+        form.action = 'https://us3.proxysite.com/includes/process.php?action=update';
+        break;
+      case 'us4':
+        form.action = 'https://us4.proxysite.com/includes/process.php?action=update';
+        break;
+      case 'us5':
+        form.action = 'https://us5.proxysite.com/includes/process.php?action=update';
+        break;
+      case 'us6':
+        form.action = 'https://us6.proxysite.com/includes/process.php?action=update';
+        break;
+      case 'us7':
+        form.action = 'https://us7.proxysite.com/includes/process.php?action=update';
+        break;
+      case 'us8':
+        form.action = 'https://us8.proxysite.com/includes/process.php?action=update';
+        break;
+      case 'us9':
+        form.action = 'https://us9.proxysite.com/includes/process.php?action=update';
+        break;
+      case 'us10':
+        form.action = 'https://us10.proxysite.com/includes/process.php?action=update';
+        break;
+      case 'us11':
+        form.action = 'https://us11.proxysite.com/includes/process.php?action=update';
+        break;
+      case 'us12':
+        form.action = 'https://us12.proxysite.com/includes/process.php?action=update';
+        break;
+      case 'us13':
+        form.action = 'https://us13.proxysite.com/includes/process.php?action=update';
+        break;
+      case 'us14':
+        form.action = 'https://us14.proxysite.com/includes/process.php?action=update';
+        break;
+      case 'us15':
+        form.action = 'https://us15.proxysite.com/includes/process.php?action=update';
+        break;
+    }
+  }
+};
